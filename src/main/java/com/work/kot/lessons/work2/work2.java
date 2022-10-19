23d8c036ace6e450d8a3d88e0ea662aed935faf0
Guzhva Valentin <guzhvavalentin@gmail.com>
2022-10-19 11:21:12 +0300

    work2 (for and white)

---
+++ src/main/java/com/work/kot/lessons/work2/work2.java
@@ -0,0 +1,106 @@
+package com.work.kot.lessons.work2;
+
+import java.util.Scanner;
+
+public class work2 {
+    public static void main(String[] args) {
+
+
+//        --- continue; ---  обрыв поточной итерациий
+//        --- break; ---  обрыв цикла
+
+//        оставить только парные числа
+//        for (int i = 0; i < 11; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Количество фрагов первой команды: ");
+//        int player1 = 0;
+//        for (;;) {
+//            if (scanner.hasNextInt()) {
+//                player1 = scanner.nextInt();
+//                break;
+//            } else {
+//                System.out.println("не верные данные");
+//                scanner.nextLine();
+//            }
+//
+//        }
+//          !!!!!!!!пропустить значение 5 и 7!!!!!!!
+//        for (int i = 1; i < 11; i++) {
+//            if (i == 5 || i == 7) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+//         !!!!!!!!пропустить значение 5 и 7 и обрыв цикла после 8!!!!!!!
+//        int i = 0;
+//        while (i < 10) {
+//            i++;
+//            if ((i == 5) || (i == 7)) {
+//                continue;
+//            }
+//            if (i == 8) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+//        int i = 11;
+//        while (i > -1) {
+//            i--;
+//            if ((int) (Math.random() * 101) >=85){
+//                System.out.println("обезврежена");
+//                break;
+//            }
+//
+//                if (i == 0) {
+//                System.out.println(i + " BOOM");
+//                i--;
+//            } else {
+//                System.out.println(i + " ты справился");
+//            }
+//
+//
+//        }
+//        ----- посчитать факторил в диапазоне от 1 до 10 --------
+//        Scanner scanner = new Scanner(System.in);
+//        int N = 0;
+//
+//        while (true) {
+//            if (scanner.hasNextInt()) {
+//                N = scanner.nextInt();
+//                if (N < 0 || N > 10) {
+//                    System.out.println("введи число от 1 до 10");
+//
+//                } else {
+//                    break;
+//                }
+//            } else {
+//                System.out.println("попробуй еще раз");
+//                scanner.nextLine();
+//            }
+//        }
+//        int result = 1;
+//        for (int i = 1; i <= N; i++) {
+//            result *= i;
+//        }
+//        System.out.println(result);
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
